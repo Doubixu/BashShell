1ee5ee51f969ac9994775ef50e571d16dcfd4843
Doubixu <97xulinux@gmail.com>
2016-09-29 00:00:21 +0800

    Update NGINX_INSTALL.sh

--- NGINX_INSTALL.sh
+++ NGINX_INSTALL.sh
@@ -14,10 +14,10 @@ function environment() {
     # getUrl='http://172.16.1.1\nginx-1.8.1.tar.gz'
     wget -P /tmp/ $getUrl/nginx.tar.gz
     grep "nginx" /etc/passwd > /dev/null
-    if [[ $? -ne 0 ]]; then  # check user and group
-        groupadd nginx
-        useradd -M -g nginx -s /sbin/nologin nginx
-    fi
+    	if [[ $? -ne 0 ]]; then  # check user and group
+            groupadd nginx
+            useradd -M -g nginx -s /sbin/nologin nginx
+    	fi
     cd /tmp; tar -zxf nginx.tar.gz; cd nginx
     return 0
 }; environment; [ $? -ne 0 ] && exit 1
@@ -35,14 +35,14 @@ function install() {
         make &> /tmp/nginx_install.log
         make install &> /tmp/nginx_install.log
         if [[ $? -ne 0 ]]; then
-            return 1
+	    return 1
         fi
-        return 0
     fi
+    return 0
 }; install; [ $? -ne 0 ] && exit 1
 
 function optimize() {
-	ln -s /usr/local/nginx/sbin/* /usr/local/sbin/ > /dev/null
+    ln -s /usr/local/nginx/sbin/* /usr/local/sbin/ > /dev/null
     cp -f /tmp/nginx_control.sh /etc/init.d/nginx
     cp -f /tmp/nginx.conf /usr/local/nginx/conf/nginx.conf
     # The number of CPU cores current server,
